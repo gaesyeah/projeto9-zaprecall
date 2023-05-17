bfc611cdd61443b0b05b6e9fd64ebcd2f09b794f
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-05-17 15:54:59 -0300

    hotfix

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import Reset from './styledReset.jsx'
 
-import './css/style.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Reset />
